9eb5622bdf4eb4e4614a592d01fc3539c825fd37
nubisub <hendraksm18@mail.ugm.ac.id>
2022-08-04 14:14:12 +0700

    add API from spoonacular

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Navbar from "./Navbar.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import Pricing from "./Pricing.jsx";
 import Recipes from "./Recipes.jsx";
 import Home from "./Home.jsx";
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/" element={<Navbar />}>
                   <Route index element={<Home />} />
@@ -18,7 +20,7 @@ function App() {
                   <Route path="Pricing" element={<Pricing />} />
               </Route>
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   )
 }
 
